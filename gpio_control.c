dc9f42ef8db80867bf1c2575fab8140d351682a6
Andy Walsh <andy@e-walsh.net>
2021-04-04 12:59:13 -0400

    Added pin validation.
    Disclaimer: This is very rough, but it's a starting point.
    
    The program will now make sure the requested pin is a general-purpose
    GPIO pin and not intended for other uses.

--- gpio_control.c
+++ gpio_control.c
@@ -1,7 +1,71 @@
 #include <getopt.h>
+#include <pigpio.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pigpio.h>
+#include <string.h>
+
+// Currently the acceptable pins to use for general purpoase GPIO signals are
+// listed below.
+int GPIO_SIZE = 10;
+int gpio[10] = { 5, 6, 16, 17, 22, 23, 24, 25, 26, 27 };
+
+/**
+ * Assemble the list of acceptable pins in a comma-formatted (not separated)
+ * list.
+ **/
+const char *print_csv_pins(char* result)
+{
+        for (int i = 0; i < GPIO_SIZE; i++)
+        {
+                char ftext[255];
+                if ((i + 1) == GPIO_SIZE)
+                {
+                        int appended = snprintf(ftext, 255, "%i", gpio[i]);
+                } else {
+                        int appended = snprintf(ftext, 255, "%i, ", gpio[i]);
+                }
+                strcat(result, ftext);
+        }
+}
+
+/**
+ * Print the usage for the program...
+ **/
+void printUsage(char arg[255])
+{
+        char csv_pins[1024];
+        print_csv_pins(csv_pins);
+        fprintf(stderr, "Usage: %s [-d NUM] [-p NUM]\n", arg);
+        fprintf(stderr, "\t-d\t Time (in seconds) to keep the pin activated\n");
+        fprintf(stderr, "\t-p\t The pin to activate (MUST BE one of %s)\n", csv_pins);
+        exit(0);
+}
+
+/**
+ * Verify that the pin that was requested is appropriate for this purpose.  An
+ * 'acceptable' pin is labelled generically as GPIO<num> pin from the raspberry
+ * pi reference.
+ *
+ * Example reference:
+ * https://www.raspberrypi.org/documentation/usage/gpio/images/GPIO-Pinout-Diagram-2.png
+ **/
+int validatePin(int pin, char arg[255])
+{
+        for (int i = 0; i < sizeof(gpio); i++)
+        {
+          if (gpio[i] == pin)
+          {
+            return pin;
+          }
+        }
+
+        char csv_pins[1024];
+        print_csv_pins(csv_pins);
+        fprintf(stderr, "Pin %i is not acceptable.  Must be one of %s\n", pin, csv_pins);
+        printUsage(arg);
+
+        return pin;
+}
 
 int main(int argc, char *argv[])
 {
@@ -20,13 +84,10 @@ int main(int argc, char *argv[])
                         delay = atoi(optarg);
                         break;
                 case 'p':
-                        pin = atoi(optarg);
+                        pin = validatePin(atoi(optarg), argv[0]);
                         break;
                 default:
-                        fprintf(stderr, "Usage: %s [-d NUM] [-p NUM]\n", argv[0]);
-                        fprintf(stderr, "\t-d\t Time (in seconds) to keep the pin activated\n");
-                        fprintf(stderr, "\t-p\t The pin to activate (MUST BE pins x,y,z,...)\n");
-                        exit(0);
+                        printUsage(argv[0]);
                 }
         }
         printf("Given delay %d\n", delay);
@@ -38,7 +99,7 @@ int main(int argc, char *argv[])
 
         // Sleep for the requested time.
         fprintf(stdout, "Sleeping for %d seconds\n", delay);
-        time_sleep(delay);
+        //time_sleep(delay);
 
         // Turn the supplied pin off.
         fprintf(stdout, "Turning pin %d off\n", pin);
